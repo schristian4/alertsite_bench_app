14e4eda241f6f019c6c8cd0bce6529a8f9f85f82
Spencer Christian <s.christian141@gmail.com>
2024-01-26 01:14:48 -0500

    limit size

--- pages/api/dashboard/index.js
+++ pages/api/dashboard/index.js
@@ -1,11 +1,21 @@
+import moment from 'moment'
 const monitorIDs = ['613072', '606458', '613080', '610382', '613088', '610378', '613070', '613076', '610380']
 
 const plainText = `${process.env.NEXT_PUBLIC_ALERT_USERNAME}:${process.env.NEXT_PUBLIC_ALERT_PASSWORD}`
 const BufferSession = Buffer.from(plainText).toString('base64')
 const BufferText = `Basic ` + BufferSession
+// https://www.alertsite.com/report-api/detail/C99999?devices=76981,94332&start_date=2013-01-20+00:00:00&end_date=2013-01-22+23:59:59&api_version=2&format=json
+const now = moment()
+const formattedTime = now.format('YYYY-MM-DD+hh:mm:ss')
+const newTime = now.subtract(5, 'minutes')
+const formattedNewTime = newTime.format('YYYY-MM-DD+hh:mm:ss')
 
+let newDate = `&start_date=${formattedNewTime}&end_date=${formattedTime}`
+console.log('newDate', newDate)
 async function get_monitor_data(id, rdate) {
-  const monitorURL = `${process.env.NEXT_PUBLIC_ALERT_URL}?devices=${id}&rdate=${rdate}&api_version=2&format=json`
+  // &rdate=${rdate}
+  const monitorURL = `${process.env.NEXT_PUBLIC_ALERT_URL}?devices=${id}${newDate}&api_version=2&format=json`
+  console.log('monitorURL', monitorURL)
   const getMonitorData = await fetch(monitorURL, {
     method: 'GET',
     headers: {
