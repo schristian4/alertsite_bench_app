03d49dc8788a56ca16d1b3ea7a776cc5a1dbf991
Spencer Christian <s.christian141@gmail.com>
2024-01-30 14:20:15 -0500

    Add SpeedInsights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 import Dashboard from './dashboard/components/Dashboard'
 import { DataProvider } from './dashboard/components/providers/data-provider'
 import { ThemeProvider } from './dashboard/components/providers/theme-provider'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center ph-24 m-12 adjust-mobile'>
       <DataProvider>
         <ThemeProvider>
           <Dashboard />
+          <SpeedInsights />
         </ThemeProvider>
       </DataProvider>
     </main>
